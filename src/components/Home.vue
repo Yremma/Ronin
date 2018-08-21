14c919dd1734af4617e3bcdd3c9157b200983d7e
Yremma <yremma@gmail.com>
2018-08-21 18:48:03 -0300

    Parallax

---
+++ src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="home" style="overflow:hidden">
+        <div id="scene">                        
+            <div data-depth="1">
+                <img src="../assets/fondo1.svg" style="width:100%; opacity:0.8"/>
+            </div>             
+            <div data-depth="-0.75">
+                <img src="../assets/fondo3.svg" style="margin-top:-15%; margin-left:-20%; width:250%; opacity:0.15"/>
+            </div>
+            <div data-depth="0.5" align="center">
+                <img src="../assets/fondo2.svg" style="margin-left:20%; width:100%; opacity:0.6"/>
+            </div>
+            <div data-depth="-0.25" align="center">
+                <img src="../assets/Logo.png" style="padding-top:5%; width:70%"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+    
+import Parallax         from 'parallax-js'
+
+    
+    export default 
+    {   mounted()
+        {   var scene = document.getElementById('scene');
+            var parallaxInstance = new Parallax(scene);
+        }
+    }
+</script>
+
+
+
+<style>
+    div
+    {   font-weight: bold;
+        font-variant: small-caps;
+        color: #fff;
+    }
+    #home 
+    {   background-image: url('../assets/Pizarron.png');
+        background-size: contain;
+        background-position: center;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,42 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue              from 'vue'
+import Router           from 'vue-router'
+import ElementUI        from 'element-ui'
+import locale           from 'element-ui/lib/locale/lang/es'
+import VueCookies       from 'vue-cookies'
+import CanvasVideo      from 'vue-canvasvideo'
+
+
+import 'element-theme-default'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/display.css'
+import 'vue-canvasvideo/dist/vuecanvasvideo.min.css'
+
+
+import App              from './App.vue'
+import Home             from '@/components/Home'
+
 
 Vue.config.productionTip = false
+Vue.use(ElementUI, { locale })
+Vue.use(VueCookies)
+Vue.use(CanvasVideo)
+Vue.use(Router)
+
+
+const router = new Router({
+    mode: 'history',
+    routes: [
+        {   path: '/',              name: 'home',           component: Home,
+        },
+        {   path: '',               redirect: '/',
+        },
+        {   path: '*',              redirect: '/',
+        }
+    ]
+})
+
 
 new Vue({
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
